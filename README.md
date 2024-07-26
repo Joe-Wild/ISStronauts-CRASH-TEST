155e647c84dd1b1a52c257c756d30dda38d0b9e0
Wild <114291450+Joe-Wild@users.noreply.github.com>
2024-07-26 20:18:40 +0200

    Prompt test

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Installation instructions
 
 This app in coded in Python 3
+
+## Dependencies
+
+### Inquirer
+
+Collection of common interactive command line user interfaces
+
+https://pypi.org/project/inquirer/
--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("hello world");
+import inquirer
+
+def promptAction():
+
+    questions = [
+        inquirer.List('action',
+            message="qu'est ce que tu viens de faire?",
+            choices=[
+                "j'ai fait du sport", 
+                "j'ai lu",
+                "j'ai pris des photos", 
+                "j'ai fait l'apéro",
+                "rien, mais je veux voir mes actions"
+            ]        
+        )
+    ]
+
+    answers = inquirer.prompt(questions)
+
+    return answers.get('action')
+
+
+def storeAction(action):
+
+    print('the action "' + action + "' has been stored into the database")
+    print()
+
+def getAllStoredActions():
+
+    print("actions of today are: apero, apero , apero")
+
+
+
+while True:
+
+    action = promptAction()
+
+    if action == "rien, mais je veux voir mes actions":
+        getAllStoredActions()
+    else:
+        storeAction(action)
+
