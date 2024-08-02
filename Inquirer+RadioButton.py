9c5d6a14758ac8d9136bca6509d8ae81211c349a
Wild <114291450+Joe-Wild@users.noreply.github.com>
2024-08-02 20:19:17 +0200

    Cool!!!

--- Inquirer+RadioButton.py
+++ Inquirer+RadioButton.py
@@ -1,14 +1,29 @@
-from tkinter import*
+from tkinter import *
 
 tasks = ["1H Sport", "30'' Reading", "1H Foreign languages","30'' Pictures of Earth","2H Scientific Researches"]
 
+def order():
+    if(x.get()==0):
+        print("Sport done! Well played.")
+    elif(x.get()==1):
+        print("Reading done! Well played.")
+    elif(x.get()==2):
+        print("Learning foreign languages done! played.")
+    elif(x.get()==3):
+        print("Pictures done! Well played.")
+    elif(x.get()==4):
+        print("science done! Well played.")
+    else:
+        print("YOU SUCK")
+ 
 window = Tk()
 
-SportImage = PhotoImage(file='sport.png')
-ReadImage = PhotoImage(file='read.png')
-LanguageImage = PhotoImage(file='language.png')
-PicturesImage = PhotoImage(file='pictures.png')
-ScienceImage = PhotoImage(file='science.png')
+sportImage = PhotoImage(file='sport.png')
+readImage = PhotoImage(file='read.png')
+languageImage = PhotoImage(file='language.png')
+picturesImage = PhotoImage(file='pictures.png')
+scienceImage = PhotoImage(file='science.png')
+tasksImages = [sportImage,readImage,languageImage,picturesImage,scienceImage]
 
 x = IntVar()
 
@@ -19,8 +34,10 @@ for index in range(len(tasks)):
                               value=index,  #assigns each radioB a different value
                               padx = 25, #add padding an x axis
                               font=("Impact",29),
-                              image = tasksimages[index], #adds image to RadioB 
-                              compound = 'left'#Position of the image
+                              Image = tasksImages[index], #adds image to RadioB 
+                              compound = 'left', #Position of the image
+                              indicator = 0, #circle indicators
+                              width = 375 #Width Rb
                               )
 
     radiobutton.pack(anchor=W)
