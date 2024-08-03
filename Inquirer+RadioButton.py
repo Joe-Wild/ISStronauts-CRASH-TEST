981e9897ff3ef247c0ae9b142ccc298cc90375ed
Wild <114291450+Joe-Wild@users.noreply.github.com>
2024-08-03 19:23:58 +0200

    Class done and Todo

--- Inquirer+RadioButton.py
+++ Inquirer+RadioButton.py
@@ -2,13 +2,13 @@ from tkinter import *
 
 tasks = ["1H Sport", "30'' Reading", "1H Foreign languages","30'' Pictures of Earth","2H Scientific Researches"]
 
-def order():
+def check():
     if(x.get()==0):
         print("Sport done! Well played.")
     elif(x.get()==1):
         print("Reading done! Well played.")
     elif(x.get()==2):
-        print("Learning foreign languages done! played.")
+        print("Learning foreign languages done! Well played.")
     elif(x.get()==3):
         print("Pictures done! Well played.")
     elif(x.get()==4):
@@ -23,21 +23,23 @@ readImage = PhotoImage(file='read.png')
 languageImage = PhotoImage(file='language.png')
 picturesImage = PhotoImage(file='pictures.png')
 scienceImage = PhotoImage(file='science.png')
+
 tasksImages = [sportImage,readImage,languageImage,picturesImage,scienceImage]
 
 x = IntVar()
 
 for index in range(len(tasks)):
-    radiobutton = Radiobutton(window,
+    radiobutton = Radiobutton (window,
                               text=tasks[index], #add txt to button
                               variable=x, #groups radioBs together is they shared the equal variable
                               value=index,  #assigns each radioB a different value
                               padx = 25, #add padding an x axis
                               font=("Impact",29),
-                              Image = tasksImages[index], #adds image to RadioB 
+                              image = tasksImages[index], #adds image to RadioB 
                               compound = 'left', #Position of the image
                               indicator = 0, #circle indicators
-                              width = 375 #Width Rb
+                              width = 375, #Width Rb
+                              command= check
                               )
 
     radiobutton.pack(anchor=W)
